264584b5fe1a1bec64ad9806d258bf2f7a56a59e
Kovaben <90385895+Kovaben@users.noreply.github.com>
2021-12-02 14:57:46 +0100

    Update StudentPageController.java

--- project_bead/ediary/ediary-application/src/main/java/com/training/ediary/application/controller/StudentPageController.java
+++ project_bead/ediary/ediary-application/src/main/java/com/training/ediary/application/controller/StudentPageController.java
@@ -37,19 +37,6 @@ public class StudentPageController {
 		model.addAttribute("schoolYears", schoolYearViewTransform.schoolClassListTransform(schoolYearService.schoolYears()));
 		return "studentView/studentPage";
 	}
-	@GetMapping("/studentPage/studentData")
-    public String studentData(Model model) {
-		model.addAttribute("schoolYears", schoolYearViewTransform.schoolClassListTransform(schoolYearService.schoolYears()));
-        return "studentView/studentData";
-    }
-	@PostMapping("/studentPage/studentData")
-	public String studentPageWithMark2(@RequestParam int selectYear, Model model, HttpServletRequest request) {	
-
-		model.addAttribute("takingSubjects", takingSubjectViewTransform.takingSubjectListTransform(takingSubjectService.takingSubjectsByStudent(request, selectYear)));		
-		model.addAttribute("schoolYears", schoolYearViewTransform.schoolClassListTransform(schoolYearService.schoolYears()));
-		model.addAttribute("selectedYear",selectYear);
-		return "studentView/studentData";
-	}
 	
 	@GetMapping("/studentPage/studentAbsent")
     public String studentAbsent(Model model) {
